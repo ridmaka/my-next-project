a1de46285b9094f0326b79b009d1c944935a3949
ridmaka <ridmakarasanjana12@gmail.com>
2024-12-08 10:43:23 +0530

    create first component

---
+++ app/Login/page.jsx
@@ -0,0 +1,11 @@
+export default function LoginPage() {
+    return (
+        <div className="w-full min-h-screen bg-yellow-200 flex justify-center items-center flex-col">
+
+            <div className="flex flex-col items-center justify-center bg-red-600 rad">
+                Welcome to the Login
+            </div>
+
+        </div>
+    )
+}
